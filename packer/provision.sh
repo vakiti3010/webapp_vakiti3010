a1c8800cc06f7f0a71792513e2250f5dea4e1b0c
Vakiti98 <vakitisaikumar1998@gmail.com>
2024-02-28 23:55:06 -0500

    removed maridb installation steps

--- packer/provision.sh
+++ packer/provision.sh
@@ -3,12 +3,6 @@ sudo cp /home/csye6225/your-app.jar /opt/your-app/
 sudo chown -R csye6225:csye6225 /opt/your-app
 sudo yum install -y java-17-openjdk
 
-# Install and configure MariaDBok
-sudo dnf install mariadb-server -y
-sudo systemctl enable mariadb
-sudo systemctl start mariadb
-sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'saikumar123';"
-
 sudo cp /tmp/your-app.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable your-app.service
