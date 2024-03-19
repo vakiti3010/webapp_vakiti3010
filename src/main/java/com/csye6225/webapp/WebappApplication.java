89cac3aa5136474c154f2c2fdc8cbd8ec8888d49
vakiti3010 <145172197+vakiti3010@users.noreply.github.com>
2024-03-19 19:13:14 -0400

    Assignment6 (#38)
    * added ops agent installation
    
    * added ops agent installation
    
    * added ops agent installation
    
    * added ops agent installation
    
    * added ops agent installation
    
    * Delete app.log

--- src/main/java/com/csye6225/webapp/WebappApplication.java
+++ src/main/java/com/csye6225/webapp/WebappApplication.java
@@ -1,11 +1,14 @@
 package com.csye6225.webapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class WebappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
