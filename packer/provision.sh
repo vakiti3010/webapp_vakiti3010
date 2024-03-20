147f0b8b0c2ea5210ca23b21ba4d97641650abcc
vakiti3010 <145172197+vakiti3010@users.noreply.github.com>
2024-03-19 21:26:00 -0400

    Assignment6 (#42)
    * added ops agent installation
    
    * added ops agent installation
    
    * added ops agent installation
    
    * added ops agent installation
    
    * added ops agent installation
    
    * Delete app.log
    
    * added ops agent installation
    
    * Delete log.json
    
    * added ops agent installation
    
    * added ops agent installation
    
    * add

--- packer/provision.sh
+++ packer/provision.sh
@@ -2,6 +2,7 @@ sudo mkdir -p /opt/your-app
 sudo cp /home/csye6225/your-app.jar /opt/your-app/
 sudo chown -R csye6225:csye6225 /opt/your-app
 sudo touch /opt/your-app/app.log
+sudo chown -R csye6225:csye6225 /opt/your-app/app.log
 sudo yum install -y java-17-openjdk
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
