7e68ebc1f4ab0a967ccc14ebee74c7111a7861e7
Vakiti98 <vakitisaikumar1998@gmail.com>
2024-03-19 22:26:38 -0400

    added ops install

--- packer/provision.sh
+++ packer/provision.sh
@@ -6,9 +6,8 @@ sudo yum install -y java-17-openjdk
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-sudo cp /tmp/your-app.service /etc/systemd/system/
-sudo cp /tmp/ops-agent-config.yaml /etc/google-cloud-ops-agent/config.yaml
-sudo systemctl restart google-cloud-ops-agent
+#sudo cp /tmp/your-app.service /etc/systemd/system/
+#sudo cp /tmp/ops-agent-config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl daemon-reload
 sudo systemctl enable your-app.service
 
